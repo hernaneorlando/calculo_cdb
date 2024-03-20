9c99f2a07fb9b2b5232d52a1c893bd72eb573ba7
Hernane Cunha <hernaneorlando@gmail.com>
2024-03-20 16:44:54 -0300

    Correção do valor do CDI no arquivo de seed SQL;

--- backend/InitialDataSeed.sql
+++ backend/InitialDataSeed.sql
@@ -1,2 +1,2 @@
 insert into configuracaoCdb (id, cdi, tb, CreatedAt, UpdatedAt) values
-(newid(), 0.09, 1.08, convert(datetime,'Jan 10 2024'), getdate());
+(newid(), 0.009, 1.08, convert(datetime,'Jan 10 2024'), getdate());
