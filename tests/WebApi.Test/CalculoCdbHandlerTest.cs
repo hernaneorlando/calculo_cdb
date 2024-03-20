5c212103c72799274433d7829ca6a4597081b524
Hernane Cunha <hernaneorlando@gmail.com>
2024-03-20 18:09:57 -0300

    Ajustes nos testes unitários removendo valor aleatórios.

--- tests/WebApi.Test/CalculoCdbHandlerTest.cs
+++ tests/WebApi.Test/CalculoCdbHandlerTest.cs
@@ -12,7 +12,7 @@ public class CalculoCdbHandlerTest
     private readonly Mock<ISqlDataContext> _dataContext = new();
 
     /// <summary>
-    /// Dado um valor monetário inicial inferior a 1(um)
+    /// Dado um valor monetário inicial inferior a 1(um), como por exemplo - R$ 100,00
     /// Quando o sitema calcular o CDB
     /// Então uma exceção do tipo <see cref="ValidationErrorException"/> será lançada.
     /// </summary>
@@ -23,7 +23,7 @@ public class CalculoCdbHandlerTest
         // Cenário
         var requisicao = new CalculoCdbCommand
         {
-            ValorInicial = ObtenhaValorMonetarioInicialPositivo() * -1,
+            ValorInicial = -100M,
             QuantidadeDeMeses = 12
         };
 
@@ -52,7 +52,7 @@ public class CalculoCdbHandlerTest
         var random = new Random();
         var requisicao = new CalculoCdbCommand
         {
-            ValorInicial = ObtenhaValorMonetarioInicialPositivo(),
+            ValorInicial = 100M,
             QuantidadeDeMeses = random.Next(0, 1)
         };
 
@@ -68,12 +68,6 @@ public class CalculoCdbHandlerTest
            .WithMessage($"'{nameof(CalculoCdbCommand.QuantidadeDeMeses)}': O prazo em meses deve ser maior que 1;");
     }
 
-    private static decimal ObtenhaValorMonetarioInicialPositivo()
-    {
-        var random = new Random();
-        return (decimal)random.NextDouble() + random.Next(1000000000);
-    }
-
     /// <summary>
     /// Dado um valor monetário inicial positivo
     /// E um prazo em meses maior que 1(um)
@@ -83,7 +77,12 @@ public class CalculoCdbHandlerTest
     /// <returns><see cref="CalculoCdbDto"/></returns>
     [Theory]
     [ClassData(typeof(ObtemAliquotaPorFaixaMensal))]
-    public async Task RetornaComSucessoOResultadoDoCalculo(decimal valorInicial, int meses, decimal aliquota)
+    public async Task RetornaComSucessoOResultadoDoCalculo(
+        decimal valorInicial, 
+        int meses, 
+        decimal aliquota,
+        decimal valorBruto,
+        decimal valorLiquido)
     {
         // Cenário
         DataContextMockSetup();
@@ -104,6 +103,8 @@ public class CalculoCdbHandlerTest
         calculoDto.Should().NotBeNull();
         calculoDto.ValorBruto.Should().BeGreaterThan(0);
         calculoDto.ValorLiquido.Should().BeLessThan(calculoDto.ValorBruto);
+        Math.Round(calculoDto.ValorBruto, 2).Should().Be(valorBruto);
+        Math.Round(calculoDto.ValorLiquido, 2).Should().Be(valorLiquido);
 
         var diferencaPercentual = 1 - (calculoDto.ValorLiquido / calculoDto.ValorBruto);
         diferencaPercentual.Should().Be(aliquota);
@@ -111,7 +112,7 @@ public class CalculoCdbHandlerTest
 
     private void DataContextMockSetup()
     {
-        var cdb = new Cdb { Cdi = 0.09M, TaxaBancaria = 1.08M };
+        var cdb = new Cdb { Cdi = 0.009M, TaxaBancaria = 1.08M };
 
         var dbSetCdbMock = new Mock<DbSet<Cdb>>();
         dbSetCdbMock
@@ -131,64 +132,69 @@ public class CalculoCdbHandlerTest
             .Setup(m => m.GetEnumerator())
             .Returns(new[] { cdb }.AsQueryable().GetEnumerator());
 
-
         _dataContext.Setup(m => m.Cdb).Returns(dbSetCdbMock.Object);
     }
 
 
     private class ObtemAliquotaPorFaixaMensal : IEnumerable<object[]>
     {
-        private readonly Random random = new();
-
         public IEnumerator<object[]> GetEnumerator()
         {
-            yield return new object[]       // Alíquota do imposto de 22,5% para um prazo entre 02 e 06 meses.
+            yield return new object[]       // Alíquota do imposto de 22,5% para um prazo de 02 meses.
             {
-                100.01M,
-                random.Next(2, 7),
-                0.225M
+                1000.50M,
+                2,
+                0.225M,
+                1020.04M,
+                790.53M
             };
-            yield return new object[]
+            yield return new object[]       // Alíquota do imposto de 22,5% para um prazo de 06 meses.
             {
-                1000000000.01M,
-                random.Next(2, 7),
-                0.225M
+                1000.50M,
+                6,
+                0.225M,
+                1060.29M,
+                821.72
             };
-            yield return new object[]       // Alíquota do imposto de 20% para um prazo de até 12 meses.
+            yield return new object[]       // Alíquota do imposto de 20% para um prazo de 07 meses.
             {
-                100.01M,
-                random.Next(7, 13),
-                0.2M
+                1000.50M,
+                7,
+                0.2M,
+                1070.59M,
+                856.47M
             };
-            yield return new object[]
+            yield return new object[]       // Alíquota do imposto de 20% para um prazo de 12 meses.
             {
-                1000000000.01M,
-                random.Next(7, 13),
-                0.2M
+                1000.50M,
+                12,
+                0.2M,
+                1123.64M,
+                898.91M
             };
-            yield return new object[]       // Alíquota do imposto de 17,5% para um prazo de até 24 meses.
+            yield return new object[]       // Alíquota do imposto de 17,5% para um prazo 13 meses.
             {
-                100.01M,
-                random.Next(13, 25),
-                0.175M
+                1000.50M,
+                13,
+                0.175M,
+                1134.57M,
+                936.02M
             };
-            yield return new object[]
+            yield return new object[]       // Alíquota do imposto de 17,5% para um prazo 24 meses.
             {
-                1000000000.01M,
-                random.Next(13, 25),
-                0.175M
+                1000.50M,
+                24,
+                0.175M,
+                1261.94M,
+                1041.10M
             };
             yield return new object[]       // Alíquota do imposto de 15% para um prazo acima de 24 meses.
-            {                               // A fim de não estourar o valor máximo do tipo Decimal
-                100.01M,                    // o total de meses foi limitado em 360 meses (30 anos).
-                random.Next(25, 360),
-                0.150M
-            };
-            yield return new object[]
             {
-                1000000000.01M,
-                random.Next(25, 360),
-                0.150M
+                1000.50M,
+                360,
+                0.150M,
+                32550.84M,
+                27668.21M
             };
         }
 
